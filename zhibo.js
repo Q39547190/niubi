a0a3b58b651b172648363827782f3a0757cd0866
Q39547190 <112136869+Q39547190@users.noreply.github.com>
2022-10-19 09:56:46 +0800

    Update zhibo.js

--- zhibo.js
+++ zhibo.js
@@ -7,7 +7,7 @@
 
 [rewrite_local]
 
-^https?:\/\/cdn.xlzhuanxian1.com:[^\s]*\/live-ns\/payVideo url script-response-body
+^https:\/\/cdn.xlzhuanxian.com:[0-9]{4}\/live-ns\/payVideo url script-response-body
 https://raw.githubusercontent.com/Q39547190/niubi/main/zhibo.js
 
 [mitm]
