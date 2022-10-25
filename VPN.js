2ff069e193bfc833fa0a1a96e6a0397ddd51804d
Q39547190 <112136869+Q39547190@users.noreply.github.com>
2022-10-25 23:43:30 +0800

    Create VPN.js

---
+++ VPN.js
@@ -0,0 +1,37 @@
+/*******************************
+中车专属破解
+VPN mastre
+下载地址:
+
+*******************************
+
+[rewrite_local]
+
+^http[s]?:\/\/vmweb\.4kfreevideo\.live\/api\/(node|user/.+) url script-response-body https://raw.githubusercontent.com/Q39547190/niubi/main/VPN.js
+
+[mitm]
+hostname= vmweb.4kfreevideo.live
+
+*******************************/
+
+
+var body = $response.body;
+var url = $request.url;
+var obj = JSON.parse(body);
+
+const vip1 = '/api/user';
+
+const vip2 = '/api/node';
+
+if (url.indexOf(vip1) != -1){
+    obj.data.transfer_enable = 999991288490189;
+    obj.data.expire_time = "2099-09-09";
+    body = JSON.stringify(obj);
+}
+
+if (url.indexOf(vip2) != -1){
+    obj.data.vip = 0;
+     body = JSON.stringify(obj);
+}
+
+$done({body});
