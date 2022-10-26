e826a3aa61cfa0f33071ec3a452de428672726a9
Q39547190 <112136869+Q39547190@users.noreply.github.com>
2022-10-26 22:05:10 +0800

    Update VPN.js

--- VPN.js
+++ VPN.js
@@ -14,10 +14,26 @@ hostname= vmweb.4kfreevideo.live
 
 *******************************/
 
-let obj = JSON.parse($response.body);
+var body = $response.body;
+var url = $request.url;
+var obj = JSON.parse(body);
 
-obj.data.transfer_enable = 999991288490189;
-obj.data.expire_time = "2099-09-09";
-obj.data.vip = 0;
-$done({ body: JSON.stringify(obj) });
+const vip1 = '/api/user';
 
+const vip2 = '/api/node';
+
+if (url.indexOf(vip1) != -1){
+    obj.data.transfer_enable = 999991288490189;
+    obj.data.expire_time = "2099-09-09";
+    body = JSON.stringify(obj);
+}
+
+if (url.indexOf(vip2) != -1){
+var hack = /"vip":\d+/g;
+body = body.replace(hack,'"vip":0');
+$done({
+ body
+});
+}
+
+$done({body});
