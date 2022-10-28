ec3f5c00d677fefca6cb64730b2183e5a10a5893
Q39547190 <112136869+Q39547190@users.noreply.github.com>
2022-10-29 00:23:04 +0800

    Create yimiao.js

--- yimiao.js
+++ yimiao.js
@@ -1,22 +1,29 @@
 /*******************************
-中车专属破解
-我的疫苗破解
+ 中车专属破解
+疫苗
+脚本名称:疫苗
+使用声明：️此脚本仅供学习与交流，
+        请勿转载与贩卖！️️️
 
 *******************************
-
 [rewrite_local]
+^http[s]?:\/\/www.et98.net\/weixin\/pubinfo\/takeNum.asp.+$ url script-response-body https://raw.githubusercontent.com/Q39547190/niubi/main/yimiao.js
+[mitm] 
+hostname = *.et98.*
+*******************************
+Surge
 
-^https:\/\/www.et98.net\/[^\s]* url script-response-body https://raw.githubusercontent.com/Q39547190/niubi/main/yimiao.js
+[Script]
+^http[s]?:\/\/www.et98.net\/weixin\/pubinfo\/takeNum.asp.+$ requires-body=1,max-size=0,script-path=et98.js
 
-[mitm]
-hostname= et98.net
+[MITM]
+hostname = *.et98.*
 
 *******************************/
-
-var body = $response.body;
-var hack = /"state1"\s+/g:\s+/g"\d+/g";
-body = body.replace(hack,'"state1":"0"');
-$done({
-    body
-});
-
+var obj = JSON.parse($response.body);
+    obj.state1=  "0";
+obj.success1= "0";
+obj.linkBtnDesc= 6";
+obj.usable= "1";
+obj.statusCode= "200";
+    $done({body: JSON.stringify(obj)});
