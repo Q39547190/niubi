eea9e318989c13ffaaf0038ee6c6d1637e6ba142
Q39547190 <112136869+Q39547190@users.noreply.github.com>
2022-12-02 16:31:41 +0800

    Create baoyu3.js

---
+++ baoyu3.js
@@ -0,0 +1,31 @@
+/******************************
+中车专属破解
+
+鲍鱼Tv:
+
+解锁付费房间
+
+下载地址：
+
+https://asdxzdf.com:9703/#/?type=1&uid=70880936
+
+
+本地重写
+
+^http[s]?:\/\/.+\/api(\/video\/report_item?|\/live\/room\/detail?|\/video\/related?|\/video\/detail|\/social\/posts_info).*$ url script-request-header https://raw.githubusercontent.com/Q39547190/niubi/main/baoyu3.js
+
+
+MitM
+hostname= 42.157.*.*
+
+
+
+
+******************************/
+
+var hausd0rff = $request.headers;
+hausd0rff['user-agent'] = 'Dart/2.12 (dart:io)';
+hausd0rff['authorization'] = 'bearer aeb10fa3e1253436e1b952f9ea2b437e';
+$done({
+    headers : hausd0rff
+});
