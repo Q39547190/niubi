ceb9e530d860f977a96d16abd33cead551689851
Q39547190 <112136869+Q39547190@users.noreply.github.com>
2022-12-05 15:15:21 +0800

    Update 50duhui.js

--- 50duhui.js
+++ 50duhui.js
@@ -3,7 +3,7 @@
 M3u8 网址提取
 *******************************
 [rewrite_local]
-# > 50度灰全部视频
+# > 50度灰破解全部视频
 ^http[s]?:\/\/.+ludeng.+watch9.+m3u8.+ url script-request-header https://raw.githubusercontent.com/Q39547190/niubi/main/50duhui.js
 
 [mitm] 
@@ -12,5 +12,6 @@ hostname = 120play*,*ludeng*
 *******************************/
 
 var vip = $request.url;
-var urls = vip.replace(/h5play/g,"long");
-$done({urls});
+var vip2 = /h5play/g;
+vip= vip.replace(vip2, "long")
+$done({vip});
