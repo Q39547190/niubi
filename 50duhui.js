9be7b84d257bf965f2ec4be38920991a92a97dc9
Q39547190 <112136869+Q39547190@users.noreply.github.com>
2022-12-05 14:42:20 +0800

    Create 50duhui.js

---
+++ 50duhui.js
@@ -0,0 +1,16 @@
+/******************************
+中车专属破解
+M3u8 网址提取
+*******************************
+[rewrite_local]
+# > 50度灰破解全部视频
+^http[s]?:\/\/.+ludeng.+watch9.+m3u8.+ url script-request-header https://raw.githubusercontent.com/Q39547190/niubi/main/50duhui.js
+
+[mitm] 
+hostname = 120play*,*ludeng*
+
+*******************************/
+
+var vip = $request.url;
+var urls = vip.replace(/h5play/g,"long");
+$done({urls});
