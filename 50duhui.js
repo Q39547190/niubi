62d5473d2fdc18f782545341a78350c07503ff6b
Q39547190 <112136869+Q39547190@users.noreply.github.com>
2022-12-05 17:05:46 +0800

    Update 50duhui.js

--- 50duhui.js
+++ 50duhui.js
@@ -4,7 +4,7 @@ M3u8 网址提取
 *******************************
 [rewrite_local]
 # > 50度灰破解全部视频
-^http[s]?:\/\/.+ludeng.+watch9.+m3u8.+ url script-request-header https://raw.githubusercontent.com/Q39547190/niubi/main/50duhui.js
+^http[s]?:\/\/.+ludeng.+watch.+m3u8.+ url script-request-header https://raw.githubusercontent.com/Q39547190/niubi/main/50duhui.js
 
 [mitm] 
 hostname = h5play*,*ludeng*
