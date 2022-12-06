6470bd34891bffdda699f9f311917a43ccb581f8
Q39547190 <112136869+Q39547190@users.noreply.github.com>
2022-12-06 15:30:34 +0800

    Update tongyongpojie.js

--- tongyongpojie.js
+++ tongyongpojie.js
@@ -11,13 +11,18 @@ QQ传媒 2022.12.6
 
 -------分割线------
 
+七度空间 2022.12.6
+下载地址：https://sn.app002.life/af/ZGD
+
+-------分割线------
+
 *******************************
 [rewrite_local]
 # > 通用破解全部视频
 ^http[s]?:\/\/.+ludeng.+watch.+m3u8.+ url script-request-header https://raw.githubusercontent.com/Q39547190/niubi/main/tongyongpojie.js
 
 [mitm] 
-hostname = h5play*,*ludeng*
+hostname = 120play*,h5play*,*ludeng*
 
 *******************************/
 
