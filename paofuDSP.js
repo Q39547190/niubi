fcdda2128af84e4fac6bf1e55143775210c5ed5c
Q39547190 <112136869+Q39547190@users.noreply.github.com>
2022-12-16 15:27:35 +0800

    Update paofuDSP.js

--- paofuDSP.js
+++ paofuDSP.js
@@ -8,7 +8,7 @@
 
 ^https:\/\/ajk.(pfdspmehd|pfdspmdyr).com.+vid.+count.+ url request-header (\r\n)Authorization:.+(\r\n) request-header $1Authorization: eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ0aW1lc3RhbXAiOjE2NzA4NTA3MTQxMDM1NjMzMDAsInVpZCI6MzI3Nzk4ODF9.5KOoKamwJCbnhu8o5txjHL5MyKAaCrNjlCxgwEAHF6o$2
 
-^http[s]?:\/\/.+(pfdspmehd|pfdspmdyr).+m3u8.+m3u8.+ url script-request-header https://raw.githubusercontent.com/Q39547190/niubi/main/paofuDSP.js
+^http[s]?:\/\/.+(pfdspmehd|pfdspmdyr).+m3u8.+ url script-request-header https://raw.githubusercontent.com/Q39547190/niubi/main/paofuDSP.js
 
 [mitm] 
 hostname =*pfdspdrzt*,*pfdspmdyr*
