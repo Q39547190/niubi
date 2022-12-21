115a12cdf601b20c7efd475a27f5125a9cd13164
Q39547190 <112136869+Q39547190@users.noreply.github.com>
2022-12-21 17:56:05 +0800

    Update 91SP.js

--- 91SP.js
+++ 91SP.js
@@ -13,12 +13,9 @@ hostname =*ppapi*,*17gwl*,*sgyrc*,*mnghx*,*czbt*,*jtshuzhiwa*
 
 *******************************/
 
-var vip = $response.body;
-var _url = $request.$request_url;
-var qingqiuti = $request.headers;
-qingqiuti['X-AUTH-TOKEN'] = '1511aa9decd50335781baf35983bcdd79eeacff2';
-qingqiuti['X-CODESIGN'] = 'eyJhbGciOiJIUzUxMiIsImlhdCI6MTY3MDE2ODE2NCwiZXhwIjoxNjg1NzIwMTY0fQ.eyJpZCI6MTA2MDI5NDExfQ.2Zen35BbPAFjghN45rsy-eB9VNFeV816E__2BS3rLmniEyQ2DUhI2EL_LWzu5ZTLSZeUMiUAr3TOBkMU9gxXyA';
+var vip = $request.headers;
+vip['X-AUTH-TOKEN'] = '1511aa9decd50335781baf35983bcdd79eeacff2';
+vip['X-CODESIGN'] = 'eyJhbGciOiJIUzUxMiIsImlhdCI6MTY3MDE2ODE2NCwiZXhwIjoxNjg1NzIwMTY0fQ.eyJpZCI6MTA2MDI5NDExfQ.2Zen35BbPAFjghN45rsy-eB9VNFeV816E__2BS3rLmniEyQ2DUhI2EL_LWzu5ZTLSZeUMiUAr3TOBkMU9gxXyA';
 $done({
-    headers : qingqiuti
+    headers : vip
 });
-
