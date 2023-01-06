83350c71261e46c4dfb186a8f8119d880fc13dae
Q39547190 <112136869+Q39547190@users.noreply.github.com>
2023-01-06 10:52:00 +0800

    Update changKHY.js

--- changKHY.js
+++ changKHY.js
@@ -17,7 +17,7 @@ var body = $response.body;
 var url = $request.url;
 var vip = JSON.parse(body);
 if (url.indexOf("/Home/UserInfo") != -1) {
-    vip.replace(/VIP_STATUS":(\d+)/, "VIP_STATUS"\:2");
+    body.replace(/VIP_STATUS":(\d+)/, "VIP_STATUS"\:2");
     $done({ "body": body });
 }
 
