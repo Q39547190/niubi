400e51f95b3f7088e2ad863af07104006110693d
Q39547190 <112136869+Q39547190@users.noreply.github.com>
2023-01-06 12:18:37 +0800

    Update changKHY.js

--- changKHY.js
+++ changKHY.js
@@ -25,7 +25,7 @@ body = JSON.stringify(vip);
 }
 
 if (url.indexOf("/My/myVipNew") != -1) {
-    vip.data.menu["MENU_NAME"] = "中车专属破解";
+    vip.data.MENU_NAME = "中车专属破解";
 body = JSON.stringify(vip);
 }
 $done({body});
