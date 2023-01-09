93ace5115d5678cda76495dce7999a24ec16d144
Q39547190 <112136869+Q39547190@users.noreply.github.com>
2023-01-09 13:28:33 +0800

    Update xingtu.js

--- xingtu.js
+++ xingtu.js
@@ -16,6 +16,6 @@ var vip = $request.headers;
 vip['x-Tt-Token'] = '00c0dd907e79e5c345c1d7fa3a50e1d55204a4bb6d4d050bc11d69a76ffc4f2237356e8c0e42ae8998c7b10a378c769b355394db154919b9994bf4124c19b0d8a15f69e60b089d355cc65dac525e5315bf40bef23fac8d480d428ddbec931c0ca113f-1.0.1';
 vip['User-Agent'] = 'Retouch 6.8.0 rv:6.8.0.31 (iPhone; iOS 15.3.1; zh_CN) Cronet';
 $done({ 
-  headers: modifiedHeaders 
+  headers: vip 
 });
 
