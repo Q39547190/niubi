53d5f4c7a7c27f8c263d8ce8afe6b68f156b7f8f
Q39547190 <112136869+Q39547190@users.noreply.github.com>
2023-01-11 10:40:58 +0800

    Update meituxiuxiu.js

--- meituxiuxiu.js
+++ meituxiuxiu.js
@@ -11,19 +11,22 @@ hostname = *xiuxiu*
 
 ******************************/
 
-var body = $response.body;
+var $response_body = $response.body;
 var url = $request.url;
+var parse = JSON.parse($response_body);
 if (url.indexOf("/user/show.json") != -1) {
-    body = body.replace(/\"free_trial\"\:\d+/g, "\"free_trial\":1").replace(/\"vip_type\"\:\d+/g, "\"vip_type\":1").replace(/\"screen_name\"\:\".*?\"/g, "\"screen_name\":\"已破解\"");
+     $response_body = $response.body.replace(/\"free_trial\"\:\d+/g, "\"free_trial\":1").replace(/\"vip_type\"\:\d+/g, "\"vip_type\":1").replace(/\"screen_name\"\:\".*?\"/g, "\"screen_name\":\"已破解\"");
 
-$done({ "body": body });
+$response_body = JSON.stringify(parse);
 
 } 
 
-if (url.indexOf(/vip/prompt/query.json") != -1) {
-    body = body.replace(/\"home_prompt\"\:\".*?\"/g, "\"home_prompt\":\"您的会员将于2030/01/01过期\u3002\"").replace(/\"home_btn_prompt\"\:\".*?\"/g, "\"home_btn_prompt\":\"立即查看\"").replace(/\"beautify_btn_prompt\"\:\".*?\"/g, "\"beautify_btn_prompt\":\"\"").replace(/\"beautify_prompt\"\:\".*?\"/g, "\"beautify_prompt\":\"\"");
+$done({ "body": $response_body });
 
-$done({ "body": body });
+if (url.indexOf(/vip/prompt/query.json") != -1) {
+     $response_body = $response.body.replace(/\"home_prompt\"\:\".*?\"/g, "\"home_prompt\":\"您的会员将于2030/01/01过期\u3002\"").replace(/\"home_btn_prompt\"\:\".*?\"/g, "\"home_btn_prompt\":\"立即查看\"").replace(/\"beautify_btn_prompt\"\:\".*?\"/g, "\"beautify_btn_prompt\":\"\"").replace(/\"beautify_prompt\"\:\".*?\"/g, "\"beautify_prompt\":\"\"");
+
+$response_body = JSON.stringify(parse);
 
 }
-
+$done({ "body": $response_body });
