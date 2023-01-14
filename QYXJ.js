2062fcf0a06e24aa028c57ba210252bd1b85aa05
Q39547190 <112136869+Q39547190@users.noreply.github.com>
2023-01-14 10:18:34 +0800

    Update QYXJ.js

--- QYXJ.js
+++ QYXJ.js
@@ -13,6 +13,7 @@ hostname = *faceu*,*retouchpics*
 
 var vip = $request.headers;
 
+
 vip['x-Tt-Token'] = '00d92c845a0798b601cfff0ff6bc691968048463291c894309dd151edcb3f90513652a1a7f9d2e69e3296b55b0ee5e36333282064e1ae4d3ce557bd62b4c35a72bab83a51e54fb2e283b71dc720f797da603495ebb0652efb0d49f3af541deda385dd-1.0.1';
 vip['User-Agent'] = 'Beauty 5.0.0 rv:5.0.0.10 (iPhone; iOS 15.3.1; zh_CN) Cronet';
 $done({ 
