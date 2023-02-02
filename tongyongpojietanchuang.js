fca237f520c77b628d63c870f8aeafc52b9c963e
Q39547190 <112136869+Q39547190@users.noreply.github.com>
2023-02-02 10:48:34 +0800

    Update tongyongpojietanchuang.js

--- tongyongpojietanchuang.js
+++ tongyongpojietanchuang.js
@@ -91,7 +91,7 @@ PiliPili (动漫类)2022.12.7
 *******************************
 [rewrite_local]
 # > 通用破解全部视频
-^http[s]?:\/\/.+fuhuida.+watch.+m3u8.+ url script-request-header https://raw.githubusercontent.com/Q39547190/niubi/main/tongyongpojietanchuang.js
+^http[s]?:\/\/.+fuhuida.+m3u8.+ url script-request-header https://raw.githubusercontent.com/Q39547190/niubi/main/tongyongpojietanchuang.js
 
 [mitm] 
 hostname = 120play*,h5play*,*ludeng*,*fuhuida*
