ebbc83a0f6e07e76e3174d775d79eec4b582d296
Q39547190 <112136869+Q39547190@users.noreply.github.com>
2023-02-16 22:22:55 +0800

    Update 91putao.js

--- 91putao.js
+++ 91putao.js
@@ -17,12 +17,9 @@ var body = $response.body;
 var url = $request.url;
 var parse = JSON.parse(body);
 
-
-
 if (url.indexOf("mode\/(playingAdv|av|vipCenterBanner") != -1) {
-    body = body.replace(/"advUrl":".*?"/g, '"advUrl":"https://im.gurl.eu.org/file/e02bee8ccae8f450fc173.png"');
-
-$done({body);
+    body = body.replace(/"advUrl":".*?"/g, '"advUrl":"https://ims.abcimgs1.com/md/img/1675539308498wanghong.png"');
+$done({ "body": body });
 }
 
 if (url.indexOf("/popNotice/list") != -1) {
