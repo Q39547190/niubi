1f5809d4e19049ff32f70e33107a0e215e1cb878
Q39547190 <112136869+Q39547190@users.noreply.github.com>
2023-02-17 15:59:05 +0800

    Update ONE.js

--- ONE.js
+++ ONE.js
@@ -5,7 +5,7 @@ ONE一个🔞
 [rewrite_local]
 # > ONE一个🔞
 
-^https:\/\/.+(cdchuangao|21ec74).+(ad|one).+(mp4|space) url script-request-header https://raw.githubusercontent.com/Q39547190/niubi/main/ONE.js
+^https:\/\/.+(cdchuangao|21ec74|0qda82zu).+(ad|one|comment).+(mp4|space|list) url script-request-header https://raw.githubusercontent.com/Q39547190/niubi/main/ONE.js
 
 [mitm] 
 hostname = *cdchuangao*,*21ec74*
@@ -14,7 +14,9 @@ hostname = *cdchuangao*,*21ec74*
 
 var url = $request.url;
 var token = "";
+var token2 = "api.0qda82zu.com";
 var newUrl = url.replace(/-sample/, token);
+var newUrl2 = url.replace(/api.21ec74.com/, token2);
 
 if (url.indexOf("mp4") != -1) {
 $done({url: newUrl});
@@ -23,4 +25,8 @@ $done({url: newUrl});
 if (url.indexOf("/ad/space") != -1) {
 
 $notify("中车专属破解-one一个🚗","****破解代码注入成功****","永久vip破解成功‼️");
+}
+
+if (url.indexOf("/comment/list") != -1) {
+$done({url: newUrl2});
 }
