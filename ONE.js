a8b09c1cfe08ee1da9521cd31feafc95fb760420
Q39547190 <112136869+Q39547190@users.noreply.github.com>
2023-02-17 16:50:15 +0800

    Update ONE.js

--- ONE.js
+++ ONE.js
@@ -30,6 +30,12 @@ if (url.indexOf("/comment/list") != -1) {
 
 vip['Host'] = 'api.0qda82zu.com';
 vip['Referer'] = 'https://byym79.com/';
+vip['User-Agent'] = 'Mozilla/5.0 (iPhone; CPU iPhone OS 15_0 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/15.0 Mobile/15E148 Safari/604.1';
+vip['Origin'] = 'https://byym79.com/';
+vip['app-version'] = '2.2.4';
+vip['platform'] = '3';
+vip['token'] = 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOjE5NTY5NDc5LCJpc192aXNpdG9yIjoxLCJ1dWlkIjoiZjVkYTgyMzAtYWU5ZC0xMWVkLWJjYmUtZjM1MDliZjgwNWM2Iiwibmlja25hbWUiOiJcdTUzZTRcdTVkZThcdTllMjEyMzIxOTY4IiwiaXAiOiIxMTIuOTYuMTM3LjM5IiwiaWF0IjoxNjc2NjIyODk4LCJleHAiOjE2NzcyMzEyOTgsIm5iZiI6MTY3NjYyMjg5OCwic3ViIjoiYXBpLjBxZGE4Mnp1LmNvbSIsImp0aSI6IjkyOGM1YzZjOGU2YzEzMjQwMDRhMDFhYTY2YWFiM2E2In0.lveEwcKtig5MVRSS4DGdMTVshynAXRJk-Znxs696gvc';
+
 $done({
     headers : vip
 });
@@ -39,6 +45,11 @@ if (url.indexOf("/article/detail") != -1) {
 
 vip['Host'] = 'api.0qda82zu.com';
 vip['Referer'] = 'https://byym79.com/';
+vip['User-Agent'] = 'Mozilla/5.0 (iPhone; CPU iPhone OS 15_0 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/15.0 Mobile/15E148 Safari/604.1';
+vip['Origin'] = 'https://byym79.com/';
+vip['app-version'] = '2.2.4';
+vip['platform'] = '3';
+vip['token'] = 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOjE5NTY5NDc5LCJpc192aXNpdG9yIjoxLCJ1dWlkIjoiZjVkYTgyMzAtYWU5ZC0xMWVkLWJjYmUtZjM1MDliZjgwNWM2Iiwibmlja25hbWUiOiJcdTUzZTRcdTVkZThcdTllMjEyMzIxOTY4IiwiaXAiOiIxMTIuOTYuMTM3LjM5IiwiaWF0IjoxNjc2NjIyODk4LCJleHAiOjE2NzcyMzEyOTgsIm5iZiI6MTY3NjYyMjg5OCwic3ViIjoiYXBpLjBxZGE4Mnp1LmNvbSIsImp0aSI6IjkyOGM1YzZjOGU2YzEzMjQwMDRhMDFhYTY2YWFiM2E2In0.lveEwcKtig5MVRSS4DGdMTVshynAXRJk-Znxs696gvc';
 $done({
     headers : vip
 });
