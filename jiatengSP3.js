7aaec83661303e8634ea6bf47cdfb49ca596c7b8
Q39547190 <112136869+Q39547190@users.noreply.github.com>
2023-02-21 18:50:52 +0800

    Create jiatengSP3.js

---
+++ jiatengSP3.js
@@ -0,0 +1,19 @@
+/******************************
+中车专属破解
+加藤视频🔞
+*******************************
+[rewrite_local]
+# > 加藤视频🔞
+
+^https:\/\/filelong.dzeyu.com.+m3u8 url script-request-header https://raw.githubusercontent.com/Q39547190/niubi/main/jiatengSP3.js
+
+[mitm] 
+hostname = *filelong.dzeyu.com*,
+
+******************************/
+
+var url = $request.url;
+var vip = $request.headers;
+var token = "trailer";
+var newUrl = url.replace(/try/, token);
+$done({url: newUrl});
