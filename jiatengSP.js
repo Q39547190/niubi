40304332b6254bca36bad18b7a91a7845d1c31ee
Q39547190 <112136869+Q39547190@users.noreply.github.com>
2023-02-21 17:35:06 +0800

    Update jiatengSP.js

--- jiatengSP.js
+++ jiatengSP.js
@@ -22,6 +22,8 @@ var content = /"content":".*?"/g;
 var nickname = /"nickname":".*?"/g;
 var userVip = /"userVip":"\d+"/g;
 var expiredTime = /"expiredTime":\d+/g;
+var headImage = /"headImage":".*?"/g;
+var uniqueId = /"uniqueId":\d+/g;
 
 if (url.indexOf("resource/adInfoPageList?adSpaceId=1") != -1) {
 
@@ -77,7 +79,7 @@ if (url.indexOf("/user/homePage") != -1) {
 parse.data.vipPvgList = [{"code":"EXC_DISCOUNT","description":"专属折扣","flag":1,"sort":1},{"code":"YP","description":"约炮特权","flag":1,"sort":2},{"code":"VIP_VIDEO","description":"观看VIP视频","flag":1,"sort":3},{"code":"UNLIMITED_VIDEO_VIEW","description":"无限观影","flag":1,"sort":4},{"code":"HD_LINE","description":"专属高清线路","flag":1,"sort":5},{"code":"FREE_ADS","description":"免弹窗广告","flag":1,"sort":6},{"code":"SEARCH_PVG","description":"搜索特权","flag":1,"sort":7},{"code":"SKIP_ADS","description":"可跳过片头广告","flag":1,"sort":8},{"code":"RESET_SEXUAL","description":"重置性趣","flag":1,"sort":9},{"code":"COMMENT_PVG","description":"评论区特权","flag":1,"sort":10},{"code":"AVATAR_DECORATE","description":"头像装饰","flag":1,"sort":11},{"code":"VIDEO_DOWNLOAD","description":"下载视频","flag":1,"sort":12}];
 body = JSON.stringify(parse);
 
-body = body.replace(nickname,'"nickname":"中车大神"').replace(userVip,'"userVip": 1').replace(expiredTime,'"expiredTime": 32493859951000');
+body = body.replace(nickname,'"nickname": "中车大神"').replace(userVip,'"userVip": 1').replace(expiredTime,'"expiredTime": 32493859951000').replace(headImage,'"headImage": "/group1/M00/C4/93/rB8AtGP0jiWAXPElAAFa4YasvOk261.jpg"').replace(uniqueId,'"uniqueId":88888888');
 $done({
     body
 });
@@ -88,7 +90,7 @@ if (url.indexOf("/user/getUser") != -1) {
 parse.data.vipPvgList = [{"code":"EXC_DISCOUNT","description":"专属折扣","flag":1,"sort":1},{"code":"YP","description":"约炮特权","flag":1,"sort":2},{"code":"VIP_VIDEO","description":"观看VIP视频","flag":1,"sort":3},{"code":"UNLIMITED_VIDEO_VIEW","description":"无限观影","flag":1,"sort":4},{"code":"HD_LINE","description":"专属高清线路","flag":1,"sort":5},{"code":"FREE_ADS","description":"免弹窗广告","flag":1,"sort":6},{"code":"SEARCH_PVG","description":"搜索特权","flag":1,"sort":7},{"code":"SKIP_ADS","description":"可跳过片头广告","flag":1,"sort":8},{"code":"RESET_SEXUAL","description":"重置性趣","flag":1,"sort":9},{"code":"COMMENT_PVG","description":"评论区特权","flag":1,"sort":10},{"code":"AVATAR_DECORATE","description":"头像装饰","flag":1,"sort":11},{"code":"VIDEO_DOWNLOAD","description":"下载视频","flag":1,"sort":12}];
 body = JSON.stringify(parse);
 
-body = body.replace(nickname,'"nickname": "中车大神"').replace(userVip,'"userVip": 1').replace(expiredTime,'"expiredTime": 32493859951000');
+body = body.replace(nickname,'"nickname": "中车大神"').replace(userVip,'"userVip": 1').replace(expiredTime,'"expiredTime": 32493859951000').replace(headImage,'"headImage": "/group1/M00/C4/93/rB8AtGP0jiWAXPElAAFa4YasvOk261.jpg"').replace(uniqueId,'"uniqueId":88888888');
 $done({
     body
 });
