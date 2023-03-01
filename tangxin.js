0d6de6fd14a516fc6823e11fb50f4860689b9af4
Q39547190 <112136869+Q39547190@users.noreply.github.com>
2023-03-01 15:55:37 +0800

    Update tangxin.js

--- tangxin.js
+++ tangxin.js
@@ -17,12 +17,16 @@ var hot = "16bi.liyongjing.org";
 var token = "m3u8";
 var token2 = "";
 var newUrl = url.replace(/txh016.com\/h5\/m3u8\?url=/, hot).replace(/m3u8-preview/, token).replace(/&time=\d+&sign=.+/, token2);
+var headers = {
+    'Host': '16bi.liyongjing.org',
+    'User-Agent': 'StormSniffer-Extension/2254 CFNetwork/1327.0.4 Darwin/21.2.0',
+    'Connection': 'keep-alive',
+    'Accept-Encoding': 'gzip'
+};
 
-var vip = $request.headers;
-vip['Host'] = '16bi.liyongjing.org';
-vip['Range'] = '*';
-
-$done({
-    url: newUrl,
-    headers : vip
+fetch(newUrl, {
+    method: 'GET',
+    headers: headers
+}).then(function(response) {
+    $done(response);
 });
