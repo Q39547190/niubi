2fecbff3d8f0b60bd61959aa4dc323e472d5bfa4
Q39547190 <112136869+Q39547190@users.noreply.github.com>
2023-03-01 09:51:44 +0800

    Update tangxin.js

--- tangxin.js
+++ tangxin.js
@@ -16,6 +16,11 @@ var url = $request.url;
 var hot = "16bi.liyongjing.org";
 var token = "m3u8";
 var token2 = "";
-var newUrl = url.replace(/m3u8-preview/, token);
-$done({url: newUrl2});
+var newUrl = url.replace(/txh016.com\/h5/\m3u8?url=/, hot).replace(/m3u8-preview/, token).replace(/&time=\d+&sign=.+/, token2);
+$done({url: newUrl});
 
+var vip = $request.headers;
+vip['Host'] = '16bi.liyongjing.org';
+$done({
+    headers : vip
+});
