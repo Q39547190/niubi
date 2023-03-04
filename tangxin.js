ac125c06b9644493b6efffa638929d97da5fc506
Q39547190 <112136869+Q39547190@users.noreply.github.com>
2023-03-04 14:45:45 +0800

    Update tangxin.js

--- tangxin.js
+++ tangxin.js
@@ -6,7 +6,7 @@
 # > 糖心🔞
 #下载地址https://txcvzqdr.com?_s=C35461
 
-^https:\/\/(txh016|txk005).com\/h5\/m3u8\?url=(.+)&time=.+ - 302 https://16bi.liyongjing.org$1
+^https:\/\/txh\d+\.com\/h5\/m3u8\?url=(.+)&time=.+ - 302 https://16bi.liyongjing.org$1
 
 ^https:\/\/16bi.liyongjing.org.+m3u8.+m3u8 url script-request-header https://raw.githubusercontent.com/Q39547190/niubi/main/tangxin.js
 
