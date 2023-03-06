c0a38cf9d09c30122129379ab7babb37c6b4b731
Q39547190 <112136869+Q39547190@users.noreply.github.com>
2023-03-06 16:50:12 +0800

    Update shenhu2.js

--- shenhu2.js
+++ shenhu2.js
@@ -36,7 +36,7 @@ $done({ "body": body });
 }
 
 if (url.indexOf("/Index/Update") != -1) {
-   parse = {"status":true,"result":{"forceUpdate":false},"ad":{"active":true,"image":"https://telegraph-image.pages.dev/file/926a64c523b8cda30818a.jpg","url":"","info":{"aid":"ad_1018","close_forever":false}}};
+   parse = {"status":true,"result":{"forceUpdate":false},"ad":{"active":true,"image":"https://cdn-upyun-images.dushu365.com/16780925047b3dd56c7b43caf85e4827dc24a193b655ontm","url":"","info":{"aid":"ad_1018","close_forever":false}}};
 
 $notify("中车专属破解-神户live🚗","****破解代码注入成功****","永久vip破解成功‼️");
 
