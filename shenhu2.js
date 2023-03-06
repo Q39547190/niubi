2c9a9344579398557581b4a822f713816865fc61
Q39547190 <112136869+Q39547190@users.noreply.github.com>
2023-03-06 16:55:09 +0800

    Update shenhu2.js

--- shenhu2.js
+++ shenhu2.js
@@ -4,7 +4,7 @@
 [rewrite_local]
 # > 神户live
 
-^https:\/\/gitee\.com\/ReactJSTeams\/next\.js\/raw\/master\/examples\/nested-components\/\.gitignore url response-body .+ response-body eyJpbmZvIjp7InpoTmFtZSI6IuS4rei9pkxpdmUiLCJ6aExpdmVOYW1lIjoi5Lit6L2m5LiT5bGe56C06KejIiwic2hvcnROYW1lIjoi5Lit6L2mIiwibG9uZ05hbWUiOiLkuK3ovabkuJPlsZ7noLTop6MiLCJlbk5hbWUiOiJLb2JlIExpdmUiLCJkb21haW4iOiLkuK3ovaZZWURTIiwiemhDb21wYW55TmFtZSI6IuS4rei9puS4k+WxnkxPR09TIiwiZW5Db21wYW55TmFtZSI6IkxFU0hBUkUgQ1VMVFVSRSBMSU1JVEVEIChIT05HS09ORykifSwiaG9zdCI6eyJhcGlIb3N0IjoiaHR0cHM6Ly9hcGkuZG9tYWluLmxpdW5pdWtlamkuaGVzaHVhbmdkZS5jb20vaW5kZXgucGhwL0FwcC8iLCJ3d3dIb3N0IjoiaHR0cHM6Ly90ZWxlZ3JhcGgtaW1hZ2UucGFnZXMuZGV2LyJ9LCJhZCI6eyJhZF9pZCI6NDEsInRpbWUiOjMsImltYWdlIjoiaHR0cHM6Ly90ZWxlZ3JhcGgtaW1hZ2UucGFnZXMuZGV2L2ZpbGUvOTI2YTY0YzUyM2I4Y2RhMzA4MThhLmpwZyIsInVybCI6Imh0dHBzOi8vdGVsZWdyYXBoLWltYWdlLnBhZ2VzLmRldi9maWxlLzkyNmE2NGM1MjNiOGNkYTMwODE4YS5qcGcifX0=
+^https:\/\/gitee\.com\/ReactJSTeams\/next\.js\/raw\/master\/examples\/nested-components\/\.gitignore url response-body .+ response-body eyJpbmZvIjp7InpoTmFtZSI6IuS4rei9pkxpdmUiLCJ6aExpdmVOYW1lIjoi5Lit6L2m5LiT5bGe56C06KejIiwic2hvcnROYW1lIjoi5Lit6L2mIiwibG9uZ05hbWUiOiLkuK3ovabkuJPlsZ7noLTop6MiLCJlbk5hbWUiOiJLb2JlIExpdmUiLCJkb21haW4iOiLkuK3ovaZZWURTIiwiemhDb21wYW55TmFtZSI6IuS4rei9puS4k+WxnkxPR09TIiwiZW5Db21wYW55TmFtZSI6IkxFU0hBUkUgQ1VMVFVSRSBMSU1JVEVEIChIT05HS09ORykifSwiaG9zdCI6eyJhcGlIb3N0IjoiaHR0cHM6Ly9hcGkuZG9tYWluLmxpdW5pdWtlamkuaGVzaHVhbmdkZS5jb20vaW5kZXgucGhwL0FwcC8iLCJ3d3dIb3N0IjoiaHR0cHM6Ly90ZWxlZ3JhcGgtaW1hZ2UucGFnZXMuZGV2LyJ9LCJhZCI6eyJhZF9pZCI6NDEsInRpbWUiOjMsImltYWdlIjoiaHR0cHM6Ly9jZG4tdXB5dW4taW1hZ2VzLmR1c2h1MzY1LmNvbS8xNjc4MDkyNTA0N2IzZGQ1NmM3YjQzY2FmODVlNDgyN2RjMjRhMTkzYjY1NW9udG0iLCJ1cmwiOiJodHRwczovL2Nkbi11cHl1bi1pbWFnZXMuZHVzaHUzNjUuY29tLzE2NzgwOTI1MDQ3YjNkZDU2YzdiNDNjYWY4NWU0ODI3ZGMyNGExOTNiNjU1b250bSJ9fQ==
 
 ^https:\/\/api.domain.liuniukeji.heshuangde.com\/index.php\/App\/(User|Account|Index)\/(Info|Login|Update) url script-response-body https://raw.githubusercontent.com/Q39547190/niubi/main/shenhu2.js
 
@@ -36,7 +36,7 @@ $done({ "body": body });
 }
 
 if (url.indexOf("/Index/Update") != -1) {
-   parse = {"status":true,"result":{"forceUpdate":false},"ad":{"active":true,"image":"https://cdn-upyun-images.dushu365.com/16780925047b3dd56c7b43caf85e4827dc24a193b655ontm","url":"","info":{"aid":"ad_1018","close_forever":false}}};
+   parse = {"status":true,"result":{"forceUpdate":false},"ad":{"active":true,"image":"https://cdn-upyun-images.dushu365.com/16780925047b3dd56c7b43caf85e4827dc24a193b655ontm","url":"https://cdn-upyun-images.dushu365.com/16780925047b3dd56c7b43caf85e4827dc24a193b655ontm","info":{"aid":"ad_1018","close_forever":false}}};
 
 $notify("中车专属破解-神户live🚗","****破解代码注入成功****","永久vip破解成功‼️");
 
