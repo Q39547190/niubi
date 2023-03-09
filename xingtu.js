10b60a5fb039794a77e0d0b0c9c58bb40555fc3e
Q39547190 <112136869+Q39547190@users.noreply.github.com>
2023-03-09 08:16:19 +0800

    Update xingtu.js

--- xingtu.js
+++ xingtu.js
@@ -13,7 +13,7 @@ hostname = *faceu*,*retouchpics*
 
 var vip = $request.headers;
 
-vip['x-Tt-Token'] = '00c0dd907e79e5c345c1d7fa3a50e1d55204a4bb6d4d050bc11d69a76ffc4f2237356e8c0e42ae8998c7b10a378c769b355394db154919b9994bf4124c19b0d8a15f69e60b089d355cc65dac525e5315bf40bef23fac8d480d428ddbec931c0ca113f-1.0.1';
+vip['x-Tt-Token'] = '0015dadfedf3108ad757cd3002edbd4d9e02abeb921d666860296643fe697d427c5c63a9f2ca13e8cf1a52e4c04c3227be121051afbd0ee1fdbc712075aa491fdd6f3a01ee00dd571d4ef0c7baf60a34b9877c306954f634c4b98e0f22581f757f8cd-1.0.1';
 vip['User-Agent'] = 'Retouch 6.8.0 rv:6.8.0.31 (iPhone; iOS 15.3.1; zh_CN) Cronet';
 $done({ 
   headers: vip 
