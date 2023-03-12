d736267f5b33e17503cc252189e0092dec8cd27a
Q39547190 <112136869+Q39547190@users.noreply.github.com>
2023-03-12 10:20:55 +0800

    Update xiaofangTQ.js

--- xiaofangTQ.js
+++ xiaofangTQ.js
@@ -11,8 +11,16 @@ hostname =  *119*
 *******************************/
 
 // 获取Authorization请求头中的Token值
-var authHeader = $response.headers["Authorization"];
-  
-  // 发送通知告知用户获取Authorization成功
-  $notify("车专属破解",authHeader,"现在可以正常使用车专属破解了。");
+var authHeader = $request.headers["Authorization"];
+if (authHeader) {
+  var token = authHeader.replace("Bearer ", "");
+  // 将Token保存到指定路径下的文本文件中，以便后续的请求使用
+  $file.write({
+    "data": token, // 添加换行符
+    "path": "/path/to/消防账号.txt",
+    "append": false       // 追加到文件末尾
+  });
+}
 
+// 继续处理当前请求
+$done();
