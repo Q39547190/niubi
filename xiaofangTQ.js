a54524375fe2dce9e0d4e3ba8ec717943aca1059
Q39547190 <112136869+Q39547190@users.noreply.github.com>
2023-03-12 17:02:35 +0800

    Update xiaofangTQ.js

--- xiaofangTQ.js
+++ xiaofangTQ.js
@@ -17,22 +17,12 @@ var vip = $request.headers;
 // 获取Authorization请求头中的Token值
 var authHeader = $request.headers["Authorization"];
 
+var path = "/path/to/header.txt";
+
+$fileManager.writeString(path, vip);
+
 vip['Accept'] = '*/*';
 
-const iCloud = $drive("icloud"); // 创建一个 iCloud Drive 实例
-
-if (!iCloud.exists) { // 检查是否可用于 iCloud
-    $notify("iCloud 不可用", "请登录您的 iCloud 帐户并重试。"); // 提醒用户 iCloud 不可用
-    $done();
-}
-
-const text = "这是要保存的文本内容"; // 要保存的文本内容
-
-iCloud.write("/9999.txt", text); // 将文本内容写入名为 test.txt 的文件中
-
-$notify("文本已保存", "文本已成功保存到 iCloud Drive 中。"); // 向用户发送成功消息
-
-$done();
 
 $done({
     headers : vip
