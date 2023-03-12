d5fe91e45cb04936bbb3afe1bedcdd58045f976c
Q39547190 <112136869+Q39547190@users.noreply.github.com>
2023-03-12 10:35:56 +0800

    Update xiaofangTQ.js

--- xiaofangTQ.js
+++ xiaofangTQ.js
@@ -13,14 +13,13 @@ hostname =  *119*
 // 获取Authorization请求头中的Token值
 var authHeader = $request.headers["Authorization"];
 if (authHeader) {
-  var token = authHeader.replace("Bearer ", "");
   // 将Token保存到指定路径下的文本文件中，以便后续的请求使用
   $file.write({
-    "data": token, // 添加换行符
+    "data": authHeader,
     "path": "/path/to/消防账号.txt",
-    "append": false       // 追加到文件末尾
+    "mode": "w"  // 覆盖写入模式
   });
 }
 
 // 继续处理当前请求
-$done();
+$done({});
