c9ba0e92eef085691d2c4595aa28f52ee4c83b6a
Q39547190 <112136869+Q39547190@users.noreply.github.com>
2023-03-12 12:20:20 +0800

    Update xiaofangTQ.js

--- xiaofangTQ.js
+++ xiaofangTQ.js
@@ -5,6 +5,10 @@
 # > 消防请求头提取
 ^https:\/\/qmxfxx.119.gov.cn\/alipay\/mini\/api\/users\/activeScore url script-request-header https://raw.githubusercontent.com/Q39547190/niubi/main/xiaofangTQ.js
 
+[task]
+# 将 Authorization 保存到 iCloud 文档
+0 * * * * echo $QX_AUTHORIZATION > $HOME/Library/Mobile\ Documents/com~apple~CloudDocs/消防账号.txt
+
 [mitm] 
 hostname =  *119*
 
@@ -15,13 +19,6 @@ var authHeader = $request.headers["Authorization"];
 var vip = $request.headers;
 vip['Accept'] = '*/*';
 
-if (url.indexOf("/users/activeScore") != -1) {
-$file.write({
-    "data": authHeader,
-    "path": "/path/to/消防账号.txt",
-    "mode": "w"  // 覆盖写入模式
-  });
-}
 $notify("中车专属破解-神户live🚗","****破解代码注入成功****",authHeader);
 $done({
     headers : vip
