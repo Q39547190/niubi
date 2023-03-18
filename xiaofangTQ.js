e2d00580429a0dd332cc3f6f6574eb72aff01756
Q39547190 <112136869+Q39547190@users.noreply.github.com>
2023-03-18 09:59:30 +0800

    Update xiaofangTQ.js

--- xiaofangTQ.js
+++ xiaofangTQ.js
@@ -1,28 +1,19 @@
-/******************************
-中车专属破解
-*******************************
-[rewrite_local]
-# > 消防请求头提取
-^https:\/\/qmxfxx.119.gov.cn\/alipay\/mini\/api\/users\/activeScore url script-request-header https://raw.githubusercontent.com/Q39547190/niubi/main/xiaofangTQ.js
+// Quantumult X Rewrite
+// > 消防请求头提取
+// 匹配目标 URL
+^https://qmxfxx.119.gov.cn/alipay/mini/api/users/activeScore url script-request-header https://raw.githubusercontent.com/username/repo/main/script.js
 
-[mitm] 
-hostname =  *119*
+// 提取 Authorization，并写入 iCloud 文件
+let filePath = "消防账号.txt";
 
-*******************************/
-
-
-let filePath = "world/消防账号.txt";
-// let filePath = "消防账号.txt";
-
-
-// Write iCloud file.
-let writeContent = $request.headers["Authorization"];
+let authHeader = $request.headers["Authorization"];
 let encoder = new TextEncoder();
-let writeUint8Array = encoder.encode(writeContent);
+let authUint8Array = encoder.encode(authHeader);
 
-if ($iCloud.writeFile(writeUint8Array, filePath)) {
-    console.log("OK");
+if ($iCloud.writeFile(authUint8Array, filePath)) {
+    console.log("写入成功:", authHeader);
 } else {
-    console.log("NO");
+    console.log("写入失败:", authHeader);
 }
+
 $done();
