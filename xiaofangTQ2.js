ddeeeb3ea56b43eade7a35ced7e065765d65be8a
Q39547190 <112136869+Q39547190@users.noreply.github.com>
2023-03-23 09:31:57 +0800

    Update xiaofangTQ2.js

--- xiaofangTQ2.js
+++ xiaofangTQ2.js
@@ -11,11 +11,10 @@ hostname =  *119*
 *******************************/
 let writeContent = $request.headers["Authorization"]; // 获取 Authorization 头的值并赋给 writeContent 变量
 
-const url = "https://qmxfxx.119.gov.cn/alipay/mini/api/home/taskScord/completeTask?taskCode=CTWLPRACTICEWEEK"; // 填入签到页面的 URL
-const method = "GET"; // 签到请求的 HTTP 方法
-const headers = {}; // 填入请求头部信息
-const body = ""; // 填入请求体信息
-
+const url = "https:\/\/qmxfxx.119.gov.cn\/alipay\/mini\/api\/home\/taskScord\/completeTask?taskCode=CTWLPRACTICEWEEK";
+const method = "GET";
+const headers = {};
+const body = ""; 
 headers.Authorization = writeContent; // 将 writeContent 的值添加到请求头部信息中的 Authorization 字段中
 
 $httpClient.request({
@@ -32,4 +31,4 @@ $httpClient.request({
     }
 });
 
-$done(); // 表示执行完毕，用于告诉 Surge 脚本
+$done();
