194dcd73221cc06b7e5259cea7cbedabf769c4f0
Q39547190 <112136869+Q39547190@users.noreply.github.com>
2023-06-27 18:45:03 +0800

    更新 tangduo.js

--- tangduo.js
+++ tangduo.js
@@ -6,7 +6,7 @@
 [rewrite_local]
 # >糖豆pro
 
-^https:\/\/.+kk.+\/(v1|v2)\/(getAd|auth\/user\/userinfo|auth\/video\/check_play|auth\/bannerList) url script-response-body https://raw.githubusercontent.com/Q39547190/niubi/main/tangduo.js
+^https:\/\/.+kk.+\/(v1|v2)\/(getAd|auth\/user\/userinfo|auth\/video\/check_play|auth\/bannerList) url script-response-body https://ghproxy.com/https://raw.githubusercontent.com/Q39547190/niubi/main/tangduo.js
 
 [mitm] 
 hostname =*kkb108*,*28api*
