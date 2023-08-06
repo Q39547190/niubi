722a4cee968867a5e719f5fcb887bae84c1d4c77
Q39547190 <112136869+Q39547190@users.noreply.github.com>
2023-08-06 13:47:45 +0800

    Update BuyTQ.js

--- BuyTQ.js
+++ BuyTQ.js
@@ -5,7 +5,24 @@
 **************************************
 
 [rewrite_local]
-^https?:\/\/buy\.itunes\.apple\.com\/verifyReceipt$ url script-response-body https://raw.githubusercontent.com/chxm1023/Rewrite/main/iTunes/xiaojizhuanzhu.js
+^https?:\/\/buy\.itunes\.apple\.com\/verifyReceipt$ url script-response-body /*************************************
+
+测试
+
+**************************************
+
+[rewrite_local]
+^https?:\/\/buy\.itunes\.apple\.com\/verifyReceipt$ url script-response-body https://raw.githubusercontent.com/Q39547190/niubi/main/BuyTQ.js
+
+[mitm]
+hostname = buy.itunes.apple.com
+
+*************************************/
+
+var data = JSON.parse($response.body);
+$notify("中车专属破解", "❌❌❌获取Token失败❌❌❌", data);
+console.log(data);
+$done();
 
 [mitm]
 hostname = buy.itunes.apple.com
